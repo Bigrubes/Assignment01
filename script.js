30ba08ec3046adc50f8d4ded7c9a8f015224aa0a
Reuben Ramos <rsramos23@gmail.com>
2023-09-25 16:22:51 -0700

    finished step 14

---
+++ script.js
@@ -0,0 +1,78 @@
+//STEP 1
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+//STEP 2
+//100;//numeric literal expression
+//"Max";//string literal expression
+//true;//boolean literal expression
+//null;//null literal expression
+
+//STEP 3
+
+//STEP 4
+// let firstName
+// let lastName
+// let yourAddress
+// let yourCity
+// let yourState
+// let yourZipCode
+// let yourAge
+// let referralSource
+// let mayWeContactYou
+
+//STEP 5
+// const firstName = "Reuben";
+// const lastName = "Ramos";
+// const yourZipCode = 92126;
+
+//STEP 6
+// const firstName = "Reuben";
+
+// const info = "My First Name is " + firstName + ".  " +"My age is " + 39 + ".";
+// console.log(info)
+
+//STEP 7
+// const lastName = "Ramos";
+// const yourZipCode = 92126;
+
+// const info1 = "My Last name is " + lastName + ". " + "Answer: True" + ".";
+// console.log(info1)
+
+// const info2 = "My zip code is " + yourZipCode + ". " + "Answer: True" + ".";
+// console.log(info2)
+
+//STEP 8
+//The following string literal is not valid.  I added a \' in between the I and M 
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+//STEP 9
+// const string = null
+// console.log(string)
+
+// const num = undefined;
+// console.log(num)
+
+//STEP 10
+
+//STEP 11
+// const myName  = 'Reuben Ramos';
+// alert("Hello " + myName + "," + " welcome to the Javascript class!");
+
+//STEP 12
+// const name = "Reuben Ramos";
+// alert("Hello " + name + "," + " welcome to the Javascript class!");
+
+//STEP 13
+// const name = "Reuben Ramos";
+// const course = "Javascript"
+// alert("Hello " + name + "," + " welcome to the " + course + " class!");
+
+//STEP 14
+// const name = "Reuben Ramos";
+// const course = "Javascript"
+// alert("Hello " + name + "," + "\nWelcome to the " + course + " class!");
